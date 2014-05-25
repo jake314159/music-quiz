b953b8cc88a9aedf1fddf988e7ea443c37862044
Jacob Causon <jc29g12@soton.ac.uk>
2014-05-25 12:48:45 +0100

    add/Added license to top of files

--- scoreCalculator.py
+++ scoreCalculator.py
@@ -1,8 +1,29 @@
+#
+#   --------------------------------
+#      music-quiz  
+#      scoreCalculator.py
+#   -------------------------------- 
+#
+#        Author: Jacob Causon            
+#                April 2014 
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); 
+#    you may not use this file except in compliance with the License. 
+#    You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#   Unless required by applicable law or agreed to in writing, software distributed
+#    under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+#    CONDITIONS OF ANY KIND, either express or implied. See the License for the
+#    specific language governing permissions and limitations under the License.
+#
+#
+
 import sys
 
 
 # a limit on the max number of words to stop stupid answers breaking things
-MAX_WORDS = 300
+MAX_WORDS = 50
 
 # How many changes are needed to change one string to another
 # Implementation based on http://en.wikipedia.org/wiki/Levenshtein_distance#Iterative_with_full_matrix
@@ -34,8 +55,7 @@ def LevenshteinDistance(s1, s2):
     return matrix[len(s1)][len(s2)]
 
 
-# Checks how many words are in both strings
-# ignoring their order
+# Checks how many words are in both strings ignoring their order
 # returns a pair where (correct, total words)
 def WordsCorrect(s1, s2):
     split1 = s1.split(" ", MAX_WORDS) ##string to match
@@ -86,10 +106,10 @@ def WordsCorrectPreFtString(s1, s2):
 
     return (correctWords, toFt1)
 
+
 if len(sys.argv) < 3:
     print "Not enough paramiters!"
     exit(1)
-    
 
 s1 = sys.argv[1].lower() #string to match
 s2 = sys.argv[2].lower() #user input
